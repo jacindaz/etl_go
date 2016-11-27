f6b16c157bbcfb4aacc31b05cfad480508481bdb
Jacinda Zhong <jacindaz@gmail.com>
2016-11-26 18:06:10 -0800

    Add processing csv to main for dev testing purposes

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+    "github.com/jacinda/etl/csv_processing"
     "github.com/jacinda/etl/postgres"
 )
 
 func main() {
+    csv_processing.ProcessCSV()
     postgres.Postgres()
 }
