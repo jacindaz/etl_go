de35f781471199f81ad926f66ef88f6846eab401
Jacinda Zhong <jacindaz@gmail.com>
2016-11-26 17:10:54 -0800

    Call csv_processing package from postgres package

---
+++ postgres/postgres.go
@@ -0,0 +1,17 @@
+package postgres
+
+import (
+    "fmt"
+    "github.com/jacinda/etl/csv_processing"
+)
+
+// read in CSV
+// create schema based on filename
+// 1 schema == 1 client
+func Postgres() {
+    fmt.Print("Inside postgres function")
+
+    csv_lines := csv_processing.ReadCSV()
+    fmt.Print("\nNumber of lines in csv: ")
+    fmt.Print(csv_lines)
+}
---
+++ postgres/postgres_test.go
@@ -0,0 +1,9 @@
+package postgres
+
+import (
+    "testing"
+)
+
+func TestPostgres() {
+
+}
